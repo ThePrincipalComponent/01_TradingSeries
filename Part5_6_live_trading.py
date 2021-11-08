9a48e0843fb7e76a8e51cc3492eee966ef599166
Lee Schmalz <leeschmalz@gmail.com>
2021-11-07 22:24:36 -0600

    Part 6

--- Part5_6_live_trading.py
+++ Part5_6_live_trading.py
@@ -4,109 +4,86 @@ from binance_keys import api_key, secret_key
 from datetime import datetime, timedelta
 import time
 from binance.exceptions import *
-
-client = Client(api_key, secret_key,tld='us')
-
-def sma(data, window):
-    return(data.rolling(window = window).mean())
-
-def bollinger_band(data, sma, window, nstd):
-    std = data.rolling(window = window).std()
-    upper_band = sma + std * nstd
-    lower_band = sma - std * nstd
-    
-    return upper_band, lower_band
-
-def gather_data(symbols,start_n_hours_ago):
-    merge = False
-    for symbol in symbols:
-        klines = client.get_historical_klines(symbol=f'{symbol}USDT', 
-                                              interval=client.KLINE_INTERVAL_1HOUR, 
-                                              start_str=str(datetime.now()-timedelta(hours=start_n_hours_ago)))
-        cols = ['OpenTime',
-                f'{symbol}-USD_Open',
-                f'{symbol}-USD_High',
-                f'{symbol}-USD_Low',
-                f'{symbol}-USD_Close',
-                f'{symbol}-USD_volume',
-                'CloseTime',
-                f'{symbol}-QuoteAssetVolume',
-                f'{symbol}-NumberOfTrades',
-                f'{symbol}-TBBAV',
-                f'{symbol}-TBQAV',
-                f'{symbol}-ignore']
-
-        df = pd.DataFrame(klines,columns=cols)
-
-        if merge == True:
-            dfs = pd.merge(df,dfs,how='inner',on=['OpenTime','CloseTime'])
-        else:
-            dfs = df
-            merge = True
-
-    dfs['OpenTime'] = [datetime.fromtimestamp(ts / 1000) for ts in dfs['OpenTime']]
-    dfs['CloseTime'] = [datetime.fromtimestamp(ts / 1000) for ts in dfs['CloseTime']]
-
-    for col in dfs.columns:
-        if not 'Time' in col:
-            dfs[col] = dfs[col].astype(float)
-
-    for symbol in symbols:
-        dfs[f'{symbol}_sma'] = sma(dfs[f'{symbol}-USD_Close'],window=20)
-        dfs[f'{symbol}_upper_band'], dfs[f'{symbol}_lower_band'] = bollinger_band(data=dfs[f'{symbol}-USD_Close'],
-                                                                                  sma=dfs[f'{symbol}_sma'],
-                                                                                  window=20,
-                                                                                  nstd=3)
-
-    dfs.dropna(inplace=True)
-    
-    return dfs
-
-def get_states(df,symbols):
-    states = {}
-
-    for symbol in symbols:
-        if df[f'{symbol}-USD_Close'].iloc[-1] < df[f'{symbol}_lower_band'].iloc[-1]:
-            states[symbol] = 'below'
-        elif df[f'{symbol}-USD_Close'].iloc[-1] > df[f'{symbol}_upper_band'].iloc[-1]:
-            states[symbol] = 'above'
-        else:
-            states[symbol] = 'inside'
-    
-    return states
+from auxiliary_functions import *
 
 symbols = ['BTC','ETH','LTC']
 start_n_hours_ago = 48
-
 balance_unit = 'USDT'
 first = True
 
+BUY_AMOUNT_USDT = 100
+
+precision = {}
+for symbol in symbols:
+    precision[symbol] = client.get_symbol_info(f'{symbol}USDT')['quotePrecision']
+
 while True:
     if (datetime.now().second % 10 == 0) or first:
-        if (datetime.now().minute == 17 and datetime.now().second == 10) or first:
+        if (datetime.now().minute == 0 and datetime.now().second == 10) or first:
             # refresh data
             first = False
             df = gather_data(symbols,48)
             states = get_states(df,symbols)
             print('Current state of the market:')
             print(states)
-        try:
             print('\n')
+
+        try:
             if balance_unit == 'USDT': # looking to buy
                 for symbol in symbols:
                     ask_price = float(client.get_orderbook_ticker(symbol = f'{symbol}USDT')['askPrice'])
                     lower_band = df[f'{symbol}_lower_band'].iloc[-1]
-                    print(f'{symbol}: ask price {ask_price} | lower band {lower_band}')
+                    
                     if ask_price < lower_band and states[symbol] == 'inside': #buy signal
-                        print('buy')
-                        balance_unit = symbol
-                        break
+                        ######################
+                        print(f'Buy order placed:')
+                        buy_order = client.order_limit_buy(symbol=f'{symbol}USDT',
+                                                          quantity=truncate(BUY_AMOUNT_USDT / ask_price, precision[symbol]),
+                                                          price = ask_price)
+                        print(buy_order)
+
+                        start = datetime.now()
+                        while True:
+                            time.sleep(1)
+                            buy_order = client.get_order(symbol=buy_order['symbol'], orderId=buy_order['orderId'])
+
+                            seconds_since_buy = (datetime.now() - start).seconds
+
+                            # resolve buy order
+                            if float(buy_order['executedQty']) == 0 and seconds_since_buy > 60*60:
+                                # no fill
+                                client.cancel_order(symbol=buy_order['symbol'], orderId=buy_order['orderId'])
+                                print('Order not filled after 1 hour, cancelled.')
+                                print('\n')
+                                break
+
+                            if float(buy_order['executedQty']) != 0 and float(buy_order['executedQty']) != float(buy_order['origQty']) and seconds_since_buy > 60*60:
+                                # partial fill
+                                client.cancel_order(symbol=buy_order['symbol'], orderId=buy_order['orderId'])
+                                balance_unit = symbol
+                                print('Order partially filled after 1 hour, cancelled the rest and awaiting sell signal.')
+                                print('\n')
+                                break
+
+                            if float(buy_order['executedQty']) ==  float(buy_order['origQty']):
+                                # completely filled
+                                balance_unit = symbol
+                                print('Order filled:')
+                                print(buy_order)
+                                print('\n')
+                                break
+                        
+                        ######################
 
             if balance_unit != 'USDT': # looking to sell
                 bid_price = float(client.get_orderbook_ticker(symbol = f'{balance_unit}USDT')['bidPrice'])
                 upper_band = df[f'{balance_unit}_upper_band'].iloc[-1]
                 if bid_price > upper_band and states[balance_unit] == 'inside': #sell signal
-                    print('sell')
+                    ######################
+                    client.order_market_sell(symbol=buy_order['symbol'],
+                                            quantity=truncate(float(buy_order['executedQty']), precision[buy_order['symbol'].replace('USDT','')]))
+                    
+                    ######################
                     balance_unit = 'USDT'
 
             time.sleep(1)
---
+++ auxiliary_functions.py
@@ -0,0 +1,93 @@
+import pandas as pd
+from binance.client import Client
+from binance_keys import api_key, secret_key
+from datetime import datetime, timedelta
+import time
+import math
+from binance.exceptions import *
+
+client = Client(api_key, secret_key,tld='us')
+
+def truncate(number, decimals=0):
+    """
+    Returns a value truncated to a specific number of decimal places.
+    https://stackoverflow.com/questions/783897/how-to-truncate-float-values
+    credit: nullstellensatz
+    """
+    if not isinstance(decimals, int):
+        raise TypeError("decimal places must be an integer.")
+    elif decimals < 0:
+        raise ValueError("decimal places has to be 0 or more.")
+    elif decimals == 0:
+        return math.trunc(number)
+
+    factor = 10.0 ** decimals
+    return math.trunc(number * factor) / factor
+
+def sma(data, window):
+    return(data.rolling(window = window).mean())
+
+def bollinger_band(data, sma, window, nstd):
+    std = data.rolling(window = window).std()
+    upper_band = sma + std * nstd
+    lower_band = sma - std * nstd
+    
+    return upper_band, lower_band
+
+def gather_data(symbols,start_n_hours_ago):
+    merge = False
+    for symbol in symbols:
+        klines = client.get_historical_klines(symbol=f'{symbol}USDT', 
+                                              interval=client.KLINE_INTERVAL_1HOUR, 
+                                              start_str=str(datetime.now()-timedelta(hours=start_n_hours_ago)))
+        cols = ['OpenTime',
+                f'{symbol}-USD_Open',
+                f'{symbol}-USD_High',
+                f'{symbol}-USD_Low',
+                f'{symbol}-USD_Close',
+                f'{symbol}-USD_volume',
+                'CloseTime',
+                f'{symbol}-QuoteAssetVolume',
+                f'{symbol}-NumberOfTrades',
+                f'{symbol}-TBBAV',
+                f'{symbol}-TBQAV',
+                f'{symbol}-ignore']
+
+        df = pd.DataFrame(klines,columns=cols)
+
+        if merge == True:
+            dfs = pd.merge(df,dfs,how='inner',on=['OpenTime','CloseTime'])
+        else:
+            dfs = df
+            merge = True
+
+    dfs['OpenTime'] = [datetime.fromtimestamp(ts / 1000) for ts in dfs['OpenTime']]
+    dfs['CloseTime'] = [datetime.fromtimestamp(ts / 1000) for ts in dfs['CloseTime']]
+
+    for col in dfs.columns:
+        if not 'Time' in col:
+            dfs[col] = dfs[col].astype(float)
+
+    for symbol in symbols:
+        dfs[f'{symbol}_sma'] = sma(dfs[f'{symbol}-USD_Close'],window=20)
+        dfs[f'{symbol}_upper_band'], dfs[f'{symbol}_lower_band'] = bollinger_band(data=dfs[f'{symbol}-USD_Close'],
+                                                                                  sma=dfs[f'{symbol}_sma'],
+                                                                                  window=20,
+                                                                                  nstd=3)
+
+    dfs.dropna(inplace=True)
+    
+    return dfs
+
+def get_states(df, symbols):
+    states = {}
+
+    for symbol in symbols:
+        if df[f'{symbol}-USD_Close'].iloc[-1] < df[f'{symbol}_lower_band'].iloc[-1]:
+            states[symbol] = 'below'
+        elif df[f'{symbol}-USD_Close'].iloc[-1] > df[f'{symbol}_upper_band'].iloc[-1]:
+            states[symbol] = 'above'
+        else:
+            states[symbol] = 'inside'
+            
+    return states
